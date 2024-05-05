5da65d9d64ecf0eaf2af7d24b3f41a65de250627
kingomez21 <juansebastiangomezmezu@gmail.com>
2024-05-04 22:14:16 -0500

    arreglos en las rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import {BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Login from './pages/login'
 import Register from './pages/register'
 import NavigateAppBar from "./NavigateAppBar";
+import { useEffect, useState } from 'react'
 
 function App() {
-  
+
+  const [token, ] = useState(localStorage.getItem('token'))
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (token === undefined || token === null) navigate("/login")
+  }, [])
+
   return (
-    <BrowserRouter>
+    <>
       <Routes>
-        <Route path="*" element={<NavigateAppBar />}/>
+        <Route path="/" element={<NavigateAppBar />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
       </Routes>
-    </BrowserRouter>
+    </>
   )
 }
 
